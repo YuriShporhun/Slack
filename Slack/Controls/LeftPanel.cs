a88ad32685e33bc37d226c81cd7ca8d4081c4b0f
Юрий Шпорхун <yuripclive@live.ru>
2022-06-16 18:00:17 +0300

    Добавил панель слева.

---
+++ Slack/Controls/LeftPanel.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace Slack.UI.WPF.Controls;
+
+public class LeftPanel : Selector
+{
+    static LeftPanel()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(LeftPanel), new FrameworkPropertyMetadata(typeof(LeftPanel)));
+    }
+}
+
--- Slack/Core/Bindable.cs
+++ Slack/Core/Bindable.cs
@@ -1,15 +1,15 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
-namespace Slack.UI.WPF.Core
-{
-    public class Bindable : INotifyPropertyChanged
-    {
-        public event PropertyChangedEventHandler? PropertyChanged;
+namespace Slack.UI.WPF.Core;
 
-        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
+public class Bindable : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
+
