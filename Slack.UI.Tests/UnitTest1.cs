15c456fdb3516417d462cc25c968a151e54ed36a
Юрий Шпорхун <yuripclive@live.ru>
2022-07-13 23:34:40 +0300

    Проект для тестов.

---
+++ Slack.UI.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Slack.UI.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
