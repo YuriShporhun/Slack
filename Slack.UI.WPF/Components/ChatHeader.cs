57d2125c86146af02d75fbf457c9be332859a724
Юрий Шпорхун <yuripclive@live.ru>
2022-07-17 23:34:24 +0300

    Небольшие изменения.

--- Slack.UI.WPF/Components/ChatHeader.cs
+++ Slack.UI.WPF/Components/ChatHeader.cs
@@ -1,25 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace Slack.UI.WPF.Controls
+namespace Slack.UI.WPF.Controls;
+
+public class ChatHeader : Control
 {
-    public class ChatHeader : Control
+    static ChatHeader()
     {
-        static ChatHeader()
-        {
-            DefaultStyleKeyProperty.OverrideMetadata(typeof(ChatHeader), new FrameworkPropertyMetadata(typeof(ChatHeader)));
-        }
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(ChatHeader), new FrameworkPropertyMetadata(typeof(ChatHeader)));
     }
 }
+
